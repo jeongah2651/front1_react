49f46c3f358f95b914974ab2ae7bdb4968c7302f
jeongah2651 <46205870+jeongah2651@users.noreply.github.com>
2024-04-04 22:21:08 +0900

    section 태그 추가

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,11 +6,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-        React를 처음 배우면서 만든 첫 프로젝트에 대한 내용과 학습 과정을 공유합니다.
-        </h1>
-        <a href="https://front1-react.vercel.app/">https://front1-react.vercel.app/</a>
       </header>
+      <section>
+        <h3> React를 처음 배우면서 만든 첫 프로젝트에 대한 내용과 학습 과정을 공유합니다.</h3>
+        <a href="https://front1-react.vercel.app/">https://front1-react.vercel.app/</a>
+      </section>
     </div>
   );
 }
