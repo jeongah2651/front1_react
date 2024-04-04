d1077003ab8fd39767d61eaec97425b0ff57c0d9
jeongah2651 <46205870+jeongah2651@users.noreply.github.com>
2024-04-04 23:53:59 +0900

    section태그 제거, h3>h1태그로 수정

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,12 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1> React를 처음 배우면서 만든 첫 프로젝트에 대한 내용과 학습 과정을 공유합니다.</h1>
         <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <section>
-        <h3> React를 처음 배우면서 만든 첫 프로젝트에 대한 내용과 학습 과정을 공유합니다.</h3>
         <a href="https://front1-react.vercel.app/">https://front1-react.vercel.app/</a>
-      </section>
+      </header>
     </div>
   );
 }
